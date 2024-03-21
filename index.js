82c0bdce9979a7a7b6662701f0ed02fd6efaa335
Ashhar <maazashhar@gmail.com>
2024-03-21 22:43:52 +0530

    fix vercel deployment

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import dotenv from 'dotenv';
 import cors from 'cors';
 import SubmissionRoute from './routes/SubmissionRoute.js';
+import bodyParser from "body-parser";
 
 
 dotenv.config();
@@ -10,6 +11,8 @@ const port = process.env.PORT;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(urlencoded({ extended: false }));
 
 app.use('/', SubmissionRoute);
 
